5ca22025c7ef67c0ef27a492d50c19ed316b587a
unknown <pham94388@gmail.com>
2023-06-06 00:29:05 +0700

    update session

--- home.php
+++ home.php
@@ -3,6 +3,8 @@ session_start();
 
 if (isset($_SESSION['username'])) {
     echo file_get_contents('./HTMLfile/home.html');
+    echo "Welcome back,".$_SESSION['username'];
+    session_write_close();
 } else {
     echo '<script> window.location.href = "login.php";alert("You need to login first")</script>';
 }
